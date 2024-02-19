e33e1abff991d9a5b416baecaadcfdec06abd953
Samson Iweibo <135668840+SamsonI95@users.noreply.github.com>
2024-02-19 05:38:22 +0100

    Update TherapistCardData1.jsx

--- src/components/Data types/TherapistCardData1.jsx
+++ src/components/Data types/TherapistCardData1.jsx
@@ -1,31 +1,31 @@
 export const data1 = [
     {
-        image: "public/Assets/Mask group5.png",
+        image: "Assets/Mask group5.png",
         name: "Dr. Olayemi Ojo",
         ratingNumber: "5.0",
     },
     {
-        image: "public/Assets/Mask group6.png",
+        image: "Assets/Mask group6.png",
         name: "Dr. Hilary James",
         ratingNumber: "5.0",
     },
     {
-        image: "public/Assets/Mask group7.png",
+        image: "Assets/Mask group7.png",
         name: "Dr. Prince Stone",
         ratingNumber: "5.0",
     },
     {
-        image: "public/Assets/Mask group8.png",
+        image: "Assets/Mask group8.png",
         name: "Dr. Chen Julian",
         ratingNumber: "5.0",
     },
     {
-        image: "public/Assets/Mask group9.png",
+        image: "Assets/Mask group9.png",
         name: "Dr. Tom Nwachukwu",
         ratingNumber: "5.0",
     },
     {
-        image: "public/Assets/Mask group10.png",
+        image: "Assets/Mask group10.png",
         name: "Dr. Alison Frank",
         ratingNumber: "5.0",
     },
