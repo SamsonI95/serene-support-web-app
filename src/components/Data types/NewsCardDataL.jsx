ee7e49f70da042c51aeb6eb6d00ea9d2df296ee8
Samson Iweibo <135668840+SamsonI95@users.noreply.github.com>
2024-02-19 05:37:29 +0100

    Update NewsCardDataL.jsx

--- src/components/Data types/NewsCardDataL.jsx
+++ src/components/Data types/NewsCardDataL.jsx
@@ -1,18 +1,18 @@
 export const newsDataL = [
     {
-      image: "public/Assets/Mask groupalt3L.svg",
+      image: "Assets/Mask groupalt3L.svg",
       title: "News 1",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
     },
     {
-      image: "public/Assets/Mask groupalt4L.svg",
+      image: "Assets/Mask groupalt4L.svg",
       title: "News 2",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
     },
     {
-      image: "public/Assets/Mask groupalt5L.svg",
+      image: "Assets/Mask groupalt5L.svg",
       title: "News 3",
       content:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ",
