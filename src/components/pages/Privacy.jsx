48cdcf9a1c7c1751c4c9ded466223987286d7930
SamsonI95 <samsoniweibo@gmail.com>
2024-03-11 20:30:34 +0100

    Mobile resposive update

--- src/components/pages/Privacy.jsx
+++ src/components/pages/Privacy.jsx
@@ -111,7 +111,7 @@ const Privacy = () => {
               />
             </div>
             <section className="-translate-x-12">
-              <div className="bg-bcg-color w-[449px] py-36 px-36">
+              <div className="bg-bcg-color w-[450px] py-36 px-36">
                 <h3 className=" text-center -translate-x-[6rem] font-semibold text-2xl w-96">
                   Get latest News by subscribing to our Newsletter
                 </h3>
